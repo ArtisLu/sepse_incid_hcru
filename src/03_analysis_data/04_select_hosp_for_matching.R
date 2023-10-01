290acc79a70c6e5da9a9310a7c9c8d4c3d551f03
ArtisLu <a.luguzis@gmail.com>
2023-10-01 07:08:40 +0300

    izmainīta atlases loģika, lai sakrīt ar rakstu

--- src/03_analysis_data/04_select_hosp_for_matching.R
+++ src/03_analysis_data/04_select_hosp_for_matching.R
@@ -7,96 +7,107 @@ library(tidyverse)
 rm(list = ls())
 
 # Dati --------------------------------------------------------------------
-load("data/03_intermediate/stac_sepsis_ind.RData")
+load("data/03_intermediate/sepsis_hosp_w_comorb.RData")
+load("data/03_intermediate/other_hosp_w_comorb.RData")
 
-# Apstrāde ----------------------------------------------------------------
 
-# atlasa 1. sepses hospitalizāciju
-tmp1 <- stac %>%
-  filter(implicit | explicit) %>% 
+# Atlase ------------------------------------------------------------------
+
+# apvieno sepses un pārējās hospitalizācijas
+all_hosp <- bind_rows(other_hosp, sepsis_hosp)
+all_hosp %>% count(implicit, explicit, tiesie, netiesie, org_trauc)
+
+# 1. SOLIS: 1) >= 18 gadi 2) sepse vai infekcija / orgānu disfunkcija 3) 2015-2019 (source population)
+source_hosp <- all_hosp %>% 
+  filter(vecums >= 18) %>% 
+  filter(implicit | explicit | netiesie) %>% # sepsis + infekcijas
+  filter(year(date1) >= 2015 & year(date1) <= 2019)
+
+nrow(source_hosp)
+# [1] 107323
+
+
+# 2. SOLIS: sadala sepses un pārējās hospitalizācijās
+sepsis_hosp <- source_hosp %>% filter(implicit | explicit)
+other_hosp <- source_hosp %>% filter(!(implicit | explicit))
+
+# pārbaude
+nrow(sepsis_hosp) + nrow(other_hosp) == nrow(source_hosp)
+# [1] TRUE
+
+# 3. SOLIS: izslēdz hospitalizācijas, kas beidzas ar nāvi
+
+# pirms
+nrow(sepsis_hosp)
+# [1] 14476
+nrow(other_hosp)
+# [1] 92847
+
+sepsis_hosp <- sepsis_hosp %>%
+  filter(izrakst_kust != "33")
+nrow(sepsis_hosp)
+# [1] 8896
+
+other_hosp <- other_hosp %>%
+  filter(izrakst_kust != "33")
+nrow(other_hosp)
+# [1] 87539
+
+# 4. SOLIS: izslēdz hospitalizācijas > 90 naktīm
+# source_hosp %>% count(is.na(date1), is.na(date2))
+# `is.na(date1)` `is.na(date2)`      n
+#   1 FALSE          FALSE          130477
+
+sepsis_hosp <- sepsis_hosp %>% 
+  filter(as.integer(date2 - date1) <= 90)
+nrow(sepsis_hosp)
+# [1] 8758
+
+other_hosp <- other_hosp %>%
+  filter(as.integer(date2 - date1) <= 90)
+nrow(other_hosp)
+# [1] 87296
+
+# 5. solis: sepses un kontroles hospitalizācijām atstāj tikai pirmo hospitalizāciju 
+sepsis_hosp <- sepsis_hosp %>% 
   arrange(date1) %>% 
-  distinct(pid, .keep_all = TRUE) %>% 
-  mutate(cohort = "sepsis")
+  distinct(pid, .keep_all = TRUE)
 
-# distinct(tmp1, eid)
+nrow(sepsis_hosp)
+# [1] 8015
 
-# atlasa hospitalizācijas pacientiem, kam nav bijis sepsis
-tmp2 <- stac %>% 
-  filter(!(pid %in% tmp1$pid)) %>% 
-  mutate(cohort = "control")
+other_hosp <- other_hosp %>% 
+  arrange(date1) %>% 
+  distinct(pid, .keep_all = TRUE)
 
-# distinct(tmp2, eid)
+nrow(other_hosp)
+# [1] 65683
 
-cohort <- bind_rows(tmp1, tmp2)
+# 6. solis: vismaz 365 dienas pēc izrakstīšanās datuma
+max_date <- as.Date("2020-12-31") - 365 # follow-up periods NEIEKĻAUJ izrakstīšanās datumu
+max_date
+# [1] "2020-01-01"
 
-# distinct(cohort, eid)
-
-# nomaina kolonnu nosaukumus
-cohort <- cohort %>% 
-  rename(vecums = vecums_hospitalizacijas_bridi) %>% 
-  rename(izrakst_kods = stac_kustiba_izrakstisanas_kustiba)
-
-# Iekļaušanas/Izslēgšanas kritēriji ---------------------------------------
-
-cohort <- cohort %>% 
-  mutate(vecums = as.integer(vecums))
-
-# trūkstoši dati kritērijiem
-
-cohort %>% 
-  count(vecums = is.na(vecums))
-
-cohort %>% 
-  count(izrakst_kods = is.na(izrakst_kods))
-
-cohort %>% 
-  count(
-    iestasanas_dat = is.na(date1),
-    izrakst_dat = is.na(date2)
-  )
-
-# nrow(cohort)
-# [1] 543540
-
-# Nav zināms vecums 
-cohort <- cohort %>% 
-  drop_na(vecums)
-# nrow(cohort)
-# [1] 535252
-
-# Vecums >= 18
-cohort <- cohort %>% 
-  filter(vecums >= 18)
-# nrow(cohort)
-# [1] 387556
-
-# saglabā visas sepses hospitalizācijas
-# -------------------------------------------------------------------------
-save(cohort, file = "data/proc/hosp_cohort_all.RData")
-# -------------------------------------------------------------------------
-
-
-# Izrakstīts dzīvs
-cohort <- cohort %>%
-  filter(izrakst_kods != "33") # Miris
-# nrow(cohort)
-# [1] 362671
-
-# Vismaz 360 dienas pirms
-min_date <- as.Date("2014-01-01") + 359 # baseline period INCLUDE index date
-
-cohort <- cohort %>% 
-  filter(date1 >= min_date)
-# nrow(cohort)
-# [1] 314066
-
-# Vismaz 360 dienas pēc
-max_date <- as.Date("2020-12-31") - 360 # follow-up period does NOT INCLUDE dishcharge date
-
-cohort <- cohort %>% 
+sepsis_hosp <- sepsis_hosp %>% 
   filter(date2 <= max_date)
-# nrow(cohort)
-# [1] 273390
+nrow(sepsis_hosp)
+# [1] 7947
+
+other_hosp <- other_hosp %>%
+  filter(date2 <= max_date)
+nrow(other_hosp)
+# [1] 65482
+
+# pārbaude, ka tikai unikāli pid
+length(unique(sepsis_hosp$pid)) == nrow(sepsis_hosp)
+length(unique(other_hosp$pid)) == nrow(other_hosp)
+
+# 7. solis: apvieno vienā datu kopā
+
+cohort <- bind_rows(
+  sepsis_hosp %>% mutate(cohort = "sepsis"),
+  other_hosp %>% mutate(cohort = "control")
+)
 
 # Pārbauda baseline un follow-up
 cohort %>% 
@@ -107,8 +118,7 @@ cohort %>%
   select(baseline, followup) %>% 
   summary()
 
-# Saglabā -----------------------------------------------------------------
-save(cohort ,file = "data/proc/hosp_cohort_alive.RData")
+# saglabā visas sepses hospitalizācijas
 # -------------------------------------------------------------------------
-
-
+save(cohort, file = "data/03_intermediate/survivor_cohort_all.RData")
+# -------------------------------------------------------------------------
